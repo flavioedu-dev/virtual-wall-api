0fa993b4e3691eed2af204d52c89a29b3f478f03
Flávio Eduardo <xflax00@gmail.com>
2023-06-09 15:48:38 -0300

    feat: added getAdminById repository

--- src/repositories/AdminRepository.ts
+++ src/repositories/AdminRepository.ts
@@ -2,13 +2,21 @@ import { prisma } from "../database/db"
 import { administratores } from "../models/Admin"
 
 export const AdminRepository = {
+  async getAdminById(id_user: number): Promise<administratores | null> {
+    const admin = await prisma.administradores.findFirst({
+      where: {
+        id_usuario: id_user
+      }
+    })
+    return admin
+  },
+  
   async createAdmin(id_user: number): Promise<administratores> {
     const admin = await prisma.administradores.create({
       data: {
         id_usuario: id_user
       }
     })
-
     return admin
-  }
+  } 
 }
